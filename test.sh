fb2aa8cfc90f826b59e383a893d699746eb8ae57
Ricardo Noguera <ricardo.noguera@telespazio.com>
2023-07-13 13:04:26 +0100

    Fix return error

--- test.sh
+++ test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 sleep 120
 GSR_ENDPOINT=$(curl http://localhost:8080/geoserver/gsr/services/?f=json)
-GSR_EXPECTED={"currentVersion":10.51,"folders":[],"services":[{"name":"Geometry","type":"GeometryServer"}]}
+GSR_EXPECTED='{"currentVersion":10.51,"folders":[],"services":[{"name":"Geometry","type":"GeometryServer"}]}'
 echo "Got $GSR_ENDPOINT"
 echo "Expected $GSR_EXPECTED"
 if [ $GSR_ENDPOINT == $GSR_EXPECTED ]; then
     echo "Success"
-    return 0
+    exit 0
 else
-    return 1
     echo "Failed"
+    exit 1
 fi
