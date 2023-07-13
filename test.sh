ea5b8c3e2a7f0fd15217f5f534248891eccfe90d
Ricardo Noguera <ricardo.noguera@telespazio.com>
2023-07-13 12:40:12 +0100

    Add test script

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sleep 120
+GSR_ENDPOINT=$(curl http://localhost:8080/geoserver/gsr/services/?f=json)
+GSR_EXPECTED={"currentVersion":10.51,"folders":[],"services":[{"name":"Geometry","type":"GeometryServer"}]}
+echo "Got $GSR_ENDPOINT"
+echo "Expected $GSR_EXPECTED"
+if [ $GSR_ENDPOINT == $GSR_EXPECTED ]: then
+    return 0
+else
+    return 1
+fi
